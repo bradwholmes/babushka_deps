6ecbb6bae8c65351e540be373f806c84c1bce0da
Brad Holmes <inkspotch@gmail.com>
2015-08-02 20:50:56 -0500

    Fix android dep

--- deps/android.rb
+++ deps/android.rb
@@ -1,26 +1,13 @@
 dep 'android' do
-#  requires 'android-sdk.bin'
+  requires 'android-sdk.bin'
   requires 'android-home environment variable'
 end
 
 dep 'android-sdk.bin' do
-  provides 'android', 'adb', 'fastboot', 'uiautomatorviewer'
+  provides 'android', 'adb', 'fastboot'
 end
 
 dep 'android-home environment variable' do 
-  requires 'bashrc-android-home environment variable'
-
-  met? {
-    puts "stuff: " + shell("echo $ANDROID_HOME")
-    !shell?("echo $ANDROID_HOME").empty?
-  }
-
-  meet {
-    shell "source ~/.bashrc"
-  }
-end
-
-dep 'bashrc-android-home environment variable' do
   android_home = '/usr/local/opt/android-sdk'
   bashrc = '~/.bashrc'
   export = "export ANDROID_HOME=#{android_home}"
@@ -32,5 +19,9 @@ dep 'bashrc-android-home environment variable' do
   meet {
     bashrc.p.touch
     bashrc.p.append(export + "\n")
+  }
+
+  after {
+    log 'Restart shell to load $ANDROID_HOME'
   }
 end
